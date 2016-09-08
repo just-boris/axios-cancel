1380a52e60c61c3711467a7a984c1985aa12c141
Rémi Koenig <remi@kayak.com>
2016-09-08 18:08:15 -0400

    Make adapter return promise

--- xhr.js
+++ xhr.js
@@ -7,157 +7,159 @@ var transformData = require('axios/lib/core/transformData');
 var isURLSameOrigin = require('axios/lib/helpers/isURLSameOrigin');
 var settle = require('axios/lib/core/settle');
 
-module.exports = function xhrAdapter(resolve, reject, config) {
-  var requestData = config.data;
-  var requestHeaders = config.headers;
-  var cancellation = config.cancellation;
+module.exports = function xhrAdapter(config) {
+  return new Promise(function(resolve, reject) {
+    var requestData = config.data;
+    var requestHeaders = config.headers;
+    var cancellation = config.cancellation;
 
-  if (cancellation.isCanceled()) {
-    // Reject with `CancellationError` so that the caller can distinguish between cancellation and failure
-    return reject(new cancel.CancellationError('Request has been canceled.'));
-  }
-
-  if (utils.isFormData(requestData)) {
-    delete requestHeaders['Content-Type']; // Let the browser set it
-  }
-
-  var request = new XMLHttpRequest();
-
-  // HTTP basic authentication
-  if (config.auth) {
-    var username = config.auth.username || '';
-    var password = config.auth.password || '';
-    requestHeaders.Authorization = 'Basic ' + btoa(username + ':' + password);
-  }
-
-  request.open(config.method.toUpperCase(), buildURL(config.url, config.params, config.paramsSerializer), true);
-
-  // Set the request timeout in MS
-  request.timeout = config.timeout;
-
-  // Listen for ready state
-  request.onreadystatechange = function handleLoad() {
-    if (!request || (request.readyState !== 4)) {
-      return;
+    if (cancellation.isCanceled()) {
+      // Reject with `CancellationError` so that the caller can distinguish between cancellation and failure
+      return reject(new cancel.CancellationError('Request has been canceled.'));
     }
 
-    // The request errored out and we didn't get a response, this will be
-    // handled by onerror instead
-    if (request.status === 0) {
-      return;
+    if (utils.isFormData(requestData)) {
+      delete requestHeaders['Content-Type']; // Let the browser set it
     }
 
-    // Prepare the response
-    var responseHeaders = 'getAllResponseHeaders' in request ? parseHeaders(request.getAllResponseHeaders()) : null;
-    var responseData = !config.responseType || config.responseType === 'text' ? request.responseText : request.response;
-    var response = {
-      data: transformData(
-        responseData,
-        responseHeaders,
-        config.transformResponse
-      ),
-      // IE sends 1223 instead of 204 (https://github.com/mzabriskie/axios/issues/201)
-      status: request.status === 1223 ? 204 : request.status,
-      statusText: request.status === 1223 ? 'No Content' : request.statusText,
-      headers: responseHeaders,
-      config: config,
-      request: request
+    var request = new XMLHttpRequest();
+
+    // HTTP basic authentication
+    if (config.auth) {
+      var username = config.auth.username || '';
+      var password = config.auth.password || '';
+      requestHeaders.Authorization = 'Basic ' + btoa(username + ':' + password);
+    }
+
+    request.open(config.method.toUpperCase(), buildURL(config.url, config.params, config.paramsSerializer), true);
+
+    // Set the request timeout in MS
+    request.timeout = config.timeout;
+
+    // Listen for ready state
+    request.onreadystatechange = function handleLoad() {
+      if (!request || (request.readyState !== 4)) {
+        return;
+      }
+
+      // The request errored out and we didn't get a response, this will be
+      // handled by onerror instead
+      if (request.status === 0) {
+        return;
+      }
+
+      // Prepare the response
+      var responseHeaders = 'getAllResponseHeaders' in request ? parseHeaders(request.getAllResponseHeaders()) : null;
+      var responseData = !config.responseType || config.responseType === 'text' ? request.responseText : request.response;
+      var response = {
+        data: transformData(
+          responseData,
+          responseHeaders,
+          config.transformResponse
+        ),
+        // IE sends 1223 instead of 204 (https://github.com/mzabriskie/axios/issues/201)
+        status: request.status === 1223 ? 204 : request.status,
+        statusText: request.status === 1223 ? 'No Content' : request.statusText,
+        headers: responseHeaders,
+        config: config,
+        request: request
+      };
+
+      settle(resolve, reject, response);
+
+      // Clean up request
+      request = null;
     };
 
-    settle(resolve, reject, response);
+    // Handle low level network errors
+    request.onerror = function handleError() {
+      // Real errors are hidden from us by the browser
+      // onerror should only fire if it's a network error
+      reject(new Error('Network Error'));
 
-    // Clean up request
-    request = null;
-  };
+      // Clean up request
+      request = null;
+    };
 
-  // Handle low level network errors
-  request.onerror = function handleError() {
-    // Real errors are hidden from us by the browser
-    // onerror should only fire if it's a network error
-    reject(new Error('Network Error'));
+    // Handle timeout
+    request.ontimeout = function handleTimeout() {
+      var err = new Error('timeout of ' + config.timeout + 'ms exceeded');
+      err.timeout = config.timeout;
+      err.code = 'ECONNABORTED';
+      reject(err);
 
-    // Clean up request
-    request = null;
-  };
-
-  // Handle timeout
-  request.ontimeout = function handleTimeout() {
-    var err = new Error('timeout of ' + config.timeout + 'ms exceeded');
-    err.timeout = config.timeout;
-    err.code = 'ECONNABORTED';
-    reject(err);
-
-    // Clean up request
-    request = null;
-  };
-
-  // Add xsrf header
-  // This is only done if running in a standard browser environment.
-  // Specifically not if we're in a web worker, or react-native.
-  if (utils.isStandardBrowserEnv()) {
-    var cookies = require('axios/lib/helpers/cookies');
+      // Clean up request
+      request = null;
+    };
 
     // Add xsrf header
-    var xsrfValue = config.withCredentials || isURLSameOrigin(config.url) ?
-        cookies.read(config.xsrfCookieName) :
-        undefined;
+    // This is only done if running in a standard browser environment.
+    // Specifically not if we're in a web worker, or react-native.
+    if (utils.isStandardBrowserEnv()) {
+      var cookies = require('axios/lib/helpers/cookies');
 
-    if (xsrfValue) {
-      requestHeaders[config.xsrfHeaderName] = xsrfValue;
+      // Add xsrf header
+      var xsrfValue = config.withCredentials || isURLSameOrigin(config.url) ?
+          cookies.read(config.xsrfCookieName) :
+          undefined;
+
+      if (xsrfValue) {
+        requestHeaders[config.xsrfHeaderName] = xsrfValue;
+      }
     }
-  }
 
-  // Add headers to the request
-  if ('setRequestHeader' in request) {
-    utils.forEach(requestHeaders, function setRequestHeader(val, key) {
-      if (typeof requestData === 'undefined' && key.toLowerCase() === 'content-type') {
-        // Remove Content-Type if data is undefined
-        delete requestHeaders[key];
-      } else {
-        // Otherwise add header to the request
-        request.setRequestHeader(key, val);
+    // Add headers to the request
+    if ('setRequestHeader' in request) {
+      utils.forEach(requestHeaders, function setRequestHeader(val, key) {
+        if (typeof requestData === 'undefined' && key.toLowerCase() === 'content-type') {
+          // Remove Content-Type if data is undefined
+          delete requestHeaders[key];
+        } else {
+          // Otherwise add header to the request
+          request.setRequestHeader(key, val);
+        }
+      });
+    }
+
+    // Add withCredentials to request if needed
+    if (config.withCredentials) {
+      request.withCredentials = true;
+    }
+
+    // Add responseType to request if needed
+    if (config.responseType) {
+      try {
+        request.responseType = config.responseType;
+      } catch (e) {
+        if (request.responseType !== 'json') {
+          throw e;
+        }
+      }
+    }
+
+    // Handle progress if needed
+    if (config.progress) {
+      if (config.method === 'post' || config.method === 'put') {
+        request.upload.addEventListener('progress', config.progress);
+      } else if (config.method === 'get') {
+        request.addEventListener('progress', config.progress);
+      }
+    }
+
+    cancellation.onCancel(function() {
+      if (request) {
+        // Abort the HTTP request
+        request.abort();
+        // Reject with `CancellationError` so that the caller can distinguish between cancellation and failure
+        reject(new cancel.CancellationError('Request has been canceled.'));
       }
     });
-  }
 
-  // Add withCredentials to request if needed
-  if (config.withCredentials) {
-    request.withCredentials = true;
-  }
-
-  // Add responseType to request if needed
-  if (config.responseType) {
-    try {
-      request.responseType = config.responseType;
-    } catch (e) {
-      if (request.responseType !== 'json') {
-        throw e;
-      }
+    if (requestData === undefined) {
+      requestData = null;
     }
-  }
 
-  // Handle progress if needed
-  if (config.progress) {
-    if (config.method === 'post' || config.method === 'put') {
-      request.upload.addEventListener('progress', config.progress);
-    } else if (config.method === 'get') {
-      request.addEventListener('progress', config.progress);
-    }
-  }
-
-  cancellation.onCancel(function() {
-    if (request) {
-      // Abort the HTTP request
-      request.abort();
-      // Reject with `CancellationError` so that the caller can distinguish between cancellation and failure
-      reject(new cancel.CancellationError('Request has been canceled.'));
-    }
+    // Send the request
+    request.send(requestData);
   });
-
-  if (requestData === undefined) {
-    requestData = null;
-  }
-
-  // Send the request
-  request.send(requestData);
 };
